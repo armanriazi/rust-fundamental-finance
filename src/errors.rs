be609a4174dfde21e61d8b52af8bc830f9db20bc
armanriazi <arman2riazi@gmail.com>
2022-12-27 14:27:34 +0000

    Arman Riazi

---
+++ src/errors.rs
@@ -0,0 +1,13 @@
+
+/// An application-specific error type
+#[derive(Debug, PartialEq, Eq)]
+pub enum AccountingError {
+    /// Account wasn't found
+    AccountNotFound(String),
+
+    /// Not enough currency in the account (underflow)
+    AccountUnderFunded(String, u64),
+
+    /// Too much currency in the account (overflow)
+    AccountOverFunded(String, u64),
+}
